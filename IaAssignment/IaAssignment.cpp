0c1a4245cc76e12156dc73615480a1f1f864855d
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2014-02-26 14:53:00 +0000

    Added an opening after the closure. Compactness condition for contours.

--- IaAssignment/IaAssignment.cpp
+++ IaAssignment/IaAssignment.cpp
@@ -9,24 +9,42 @@ Mat CropCctvBorder(Mat& image)
 	return image(Range(56, 460), Range(25, 690));
 }
 
+bool IsContourAcceptable(vector<Point>& contour)
+{
+	double area = contourArea(contour);
+	if (area < 400)
+		return false;
+
+	double perimiter = arcLength(contour, true);
+	double compactness = perimiter * perimiter / area;
+	if (compactness > 30 * CV_PI)
+		return false;
+
+	return true;
+}
+
 int main(int argc, char *argv[]) try
 {
 	Mat empty = imread("data\\empty.png", CV_LOAD_IMAGE_GRAYSCALE);
 	empty = CropCctvBorder(empty);
 
-	Mat sample = imread("samples\\all\\lc-00082.png", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat sample = imread("samples\\all\\lc-00579.png", CV_LOAD_IMAGE_GRAYSCALE);
 	sample = CropCctvBorder(sample);
 	imshow("orig", sample);
 
 	Mat image;
 	absdiff(empty, sample, image);
-	imshow("inter", image);
-
+	
 	threshold(image, image, 70, 255, THRESH_BINARY);
 
 	Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(4, 4));
 	dilate(image, image, kernel, Point(-1,-1), 3);
 	erode(image, image, kernel, Point(-1, -1), 3);
+
+	imshow("inter", image);
+
+	erode(image, image, kernel, Point(-1, -1), 3);
+	dilate(image, image, kernel, Point(-1, -1), 3);
 
 	vector<vector<Point>> contours;
 	findContours(image.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
@@ -37,8 +55,7 @@ int main(int argc, char *argv[]) try
 	for (int i = 0; i < contours.size(); i++)
 	{
 		auto& contour = contours[i];
-		double area = contourArea(contour);
-		if (area > 400.0)
+		if (IsContourAcceptable(contour))
 			drawContours(imageColour, contours, i, Scalar(0, 0, 255), CV_FILLED);
 	}
 
