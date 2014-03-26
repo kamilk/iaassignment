e12838beeb3dac2258864b43b1533e8eb8391f6c
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2014-03-26 15:17:06 +0000

    The stats starter displays now a list of wrongly classified images.

--- Starter/StatsStarter.py
+++ Starter/StatsStarter.py
@@ -9,7 +9,9 @@ if len(sys.argv) < 2:
     print 'Not enough arguments'
     exit()
 
-dir = Starter.get_individual_path(sys.argv[1])
+event_name = sys.argv[1]
+    
+dir = Starter.get_individual_path(event_name)
 path = os.path.join(dir, '*.png')
 proc = subprocess.Popen(['..\\Hand-in\\IaAssignment.exe', path], stdout = subprocess.PIPE)
 
@@ -18,18 +20,31 @@ stats = []
 for i in range(1, 6):
     stats.append(0)
 
+descriptions = ['ontrack', 'entering', 'leaving', 'barrier', 'train']
+event_number = descriptions.index(event_name) + 1
+wrongly_classified = []    
+    
 count = 0
 while True:
     line = proc.stdout.readline()
     if line == '': break
     print line.strip()
-    line = line[(line.find(':')+1):]
-    events = r.findall(line)
+    colon_idx = line.find(':')
+    file_name = line[0:colon_idx]
+    line = line[(colon_idx+1):]
+    events = [int(event) for event in r.findall(line)]
     for event in events:
-        stats[int(event) - 1] += 1
+        stats[event - 1] += 1
     count += 1
+    if event_number not in events:
+        wrongly_classified.append(file_name)
+
+print '\nImages not classified as \'', event_name, '\':'
+for image in wrongly_classified:
+    print image
+
+print
         
-descriptions = ['ontrack', 'entering', 'leaving', 'barrier', 'train']
 print 'TOTAL: ', count
 for event, occur in enumerate(stats):
     print 'Event ', event + 1, ' (', descriptions[event], ')\t: ', occur, '\t(', occur/count * 100, '%)'
